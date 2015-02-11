f1626d9e38a77db7bbafd8187493ab0ed09bc436
606 <worzepower@gmail.com>
2015-02-11 10:46:34 +0200

    Create fn_delevel.pas

---
+++ testing/fn_delevel.pas
@@ -0,0 +1,19 @@
+begin
+while 1<>2 do begin   // бесконечный цикл
+if (User.Level>ТУТ ПИШЕМ ЛВЛ,КОТОРЫЙ ВАМ НУЖЕН) and (user.Dead) then
+       begin
+       Engine.gohome;
+       Engine.MoveTo(147366, 19833, -2035);
+       end;
+begin
+  Engine.SetTarget ('Кертис');
+  Engine.Attack(1, true);
+  end;
+  end;
+  end.
+
+
+
+
+//Лучше бить скилом
+Engine.useSkill(ИД СКИЛА, true);
